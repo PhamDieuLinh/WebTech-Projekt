aaa2638a4df1343392be89f156b9645d2bafff79
PhamDieuLinh <Dieu.Pham@Student.HTW-Berlin.de>
2022-11-05 21:42:13 +0100

    entity anlegen

---
+++ src/main/java/htw/berlin/webtech/persistence/RestaurantEntity.java
@@ -0,0 +1,58 @@
+package htw.berlin.webtech.persistence;
+
+import javax.persistence.*;
+
+
+@Entity(name = "restaurants")
+public class RestaurantEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long rid;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "address")
+    private String address;
+    @Column(name = "description")
+    private String description;
+
+    public RestaurantEntity(long rid, String name, String address, String description) {
+        this.rid = rid;
+        this.name = name;
+        this.address = address;
+        this.description = description;
+    }
+
+    protected RestaurantEntity(){
+
+    }
+
+    public long getRid() {
+        return rid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
