3e39e672f56e82067d4fd3c829a43ea601c7a4bc
PhamDieuLinh <Dieu.Pham@Student.HTW-Berlin.de>
2022-11-07 18:34:10 +0100

    Interface repository

---
+++ src/main/java/htw/berlin/webtech/persistence/RestaurantRepository.java
@@ -0,0 +1,11 @@
+package htw.berlin.webtech.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface RestaurantRepository extends JpaRepository<RestaurantEntity, Long> {
+    List<RestaurantEntity> findAllByName(String name);
+}
