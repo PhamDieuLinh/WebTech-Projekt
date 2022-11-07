1c1f4269bc6197906a1c9b590408bc46b1847054
PhamDieuLinh <Dieu.Pham@Student.HTW-Berlin.de>
2022-11-07 18:43:20 +0100

    rest controller

--- src/main/java/htw/berlin/webtech/demo/RestaurantRestController.java
+++ src/main/java/htw/berlin/webtech/demo/RestaurantRestController.java
@@ -2,25 +2,26 @@ package htw.berlin.webtech.demo;
 
 
 import htw.berlin.webtech.demo.api.Restaurant;
+import htw.berlin.webtech.service.RestaurantService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class RestaurantRestController {
 
-    private List<Restaurant> restaurants;
+    private final RestaurantService restaurantService;
 
-    public RestaurantRestController() {
-        restaurants = new ArrayList<>();
-
+    public RestaurantRestController(RestaurantService restaurantService) {
+        this.restaurantService = restaurantService;
     }
 
-    @GetMapping(path = "/api")
+
+    @GetMapping(path = "/api/v1/restaurants")
     public ResponseEntity<List<Restaurant>> fetchRestaurants() {
+        var restaurants = restaurantService.findAll();
 
         return ResponseEntity.ok(restaurants);
     }
