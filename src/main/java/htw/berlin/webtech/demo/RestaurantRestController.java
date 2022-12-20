44c991a78b4d7cdb58702e9c6647ace87e407eaa
Thuong Hoang <Thuong.@htw.com>
2022-12-21 00:14:19 +0100

    Cors Config Quelle

--- src/main/java/htw/berlin/webtech/demo/RestaurantRestController.java
+++ src/main/java/htw/berlin/webtech/demo/RestaurantRestController.java
@@ -56,7 +56,7 @@ public class RestaurantRestController {
        boolean successful = restaurantService.deleteById(id) ;
        return successful ? ResponseEntity.ok().build() : ResponseEntity.notFound().build();
    }
-
+//https://www.bing.com/videos/search?q=backend+cors+enable+java&&view=detail&mid=910E19DD1F4ED0341E16910E19DD1F4ED0341E16&&FORM=VRDGAR&ru=%2Fvideos%2Fsearch%3Fq%3Dbackend%2520cors%2520enable%2520java%26qs%3Dn%26form%3DQBVDMH%26%3D%2525eIhren%2520Suchverlauf%2520verwalten%2525E%26sp%3D-1%26pq%3Dbackend%2520cors%2520enable%2520java%26sc%3D0-24%26sk%3D%26cvid%3D0630400FA2464669B076D9FCF44829E7%26ghsh%3D0%26ghacc%3D0%26ghpl%3D
     @Configuration
     public class MyConfiguration {
 
