064621274f02b280cb09c70e4c3b667456050659
Thuong Hoang <Thuong.@htw.com>
2022-12-21 13:54:02 +0100

    Cors Config

--- src/main/java/htw/berlin/webtech/demo/RestaurantRestController.java
+++ src/main/java/htw/berlin/webtech/demo/RestaurantRestController.java
@@ -4,14 +4,12 @@ package htw.berlin.webtech.demo;
 import htw.berlin.webtech.demo.api.Restaurant;
 import htw.berlin.webtech.demo.api.RestaurantManipulationRequest;
 import htw.berlin.webtech.service.RestaurantService;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -57,7 +55,6 @@ public class RestaurantRestController {
        boolean successful = restaurantService.deleteById(id) ;
        return successful ? ResponseEntity.ok().build() : ResponseEntity.notFound().build();
    }
-//https://www.bing.com/videos/search?q=backend+cors+enable+java&&view=detail&mid=910E19DD1F4ED0341E16910E19DD1F4ED0341E16&&FORM=VRDGAR&ru=%2Fvideos%2Fsearch%3Fq%3Dbackend%2520cors%2520enable%2520java%26qs%3Dn%26form%3DQBVDMH%26%3D%2525eIhren%2520Suchverlauf%2520verwalten%2525E%26sp%3D-1%26pq%3Dbackend%2520cors%2520enable%2520java%26sc%3D0-24%26sk%3D%26cvid%3D0630400FA2464669B076D9FCF44829E7%26ghsh%3D0%26ghacc%3D0%26ghpl%3D
     @Configuration
     @EnableWebMvc
     public class WebConfig implements WebMvcConfigurer {
